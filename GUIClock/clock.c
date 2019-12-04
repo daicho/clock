7d4d079e6b8690498d1590934844728230731238
daicho <daichimax78@gmail.com>
2019-12-04 12:27:20 +0900

    周期をなめらかに

--- GUIClock/clock.c
+++ GUIClock/clock.c
@@ -13,21 +13,15 @@ void Reshape(int, int);
 void Timer(int);
 void drawCircle(double, double, double, int);
 void drawGear(double, double, double, double, double, int);
+void drawKana(double, double, double, double, double, int);
+void drawGangi(double, double, double, double, double, int);
 void drawHand(double, double, double, double);
 void drawPendulum(double, double, double, double);
 
-double start_time;
-double hour = 0;
-double min = 0;
-double sec = 0;
-double t = 0;
+// 時刻
+long double t;
 
 int main(int argc, char *argv[]) {
-    // 開始時間を取得
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    start_time = tv.tv_sec + tv.tv_usec * 1e-6;
-
     // 初期化・設定
     glutInit(&argc, argv);
     glutInitWindowSize(640, 640);
@@ -38,17 +32,32 @@ int main(int argc, char *argv[]) {
     glClearColor(1.0, 1.0, 1.0, 1.0);
 
     // アンチエイリアスを有効
-    glEnable(GL_LINE_SMOOTH);
     glEnable(GL_BLEND);
-	glBlendFunc(GL_SRC_ALPHA , GL_ONE_MINUS_SRC_ALPHA);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    glutTimerFunc(1000.0 / FPS, Timer, 0);
+    glEnable(GL_LINE_SMOOTH);
+    glEnable(GL_POLYGON_SMOOTH);
+    glHint(GL_LINE_SMOOTH_HINT, GL_NICEST);
+    glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
+
+    Timer(0);
     glutMainLoop();
 
     return 0;
 }
 
 void Display(void) {
+    time_t tv_sec = (int)t;
+    struct tm *t_st = localtime(&tv_sec);
+
+    int tictac = (t - (int)t) * 2;
+    double decimal = (t - (int)t) * 2 - tictac;
+    double tp = (int)t + (pow(decimal, 3) + tictac) / 2;
+
+    double sec = t_st->tm_sec + tp - (int)t;
+    double min = t_st->tm_min + sec / 60;
+    double hour = t_st->tm_hour + min / 60;
+
     // 中心座標
     double x0 = 0;
     double y0 = 0.4;
@@ -72,14 +81,18 @@ void Display(void) {
     double r2 = r * 2 * sqrt(15);
     double r3 = r * 2 * sqrt(15);
     double r4 = r * 6;
+    double r5 = r * 2 * sqrt(3);
+    double r6 = r * 2 * sqrt(3);
     double rg = r * 5;
     double rp = r * 50;
 
     // 歯車の角速度
-    double w1 = 2 * M_PI / 3600;
+    double w1 = 2 * M_PI / 36000;
     double w2 = -2 * M_PI / 3600;
     double w3 = 2 * M_PI / (120 * sqrt(15));
     double w4 = -2 * M_PI / 60;
+    double w5 = 2 * M_PI / (7200 * sqrt(3));
+    double w6 = -2 * M_PI / 43200;
     double wg = 2 * M_PI / 12;
     double wp = 2 * M_PI;
 
@@ -94,22 +107,31 @@ void Display(void) {
     // 歯車描画
     drawCircle(x0, y0, 0.5, 100);
 
-    drawGear(x0 - r1 - r, y0, t * w1, r1, l, 80);
-    drawGear(x0 - r1 - r, y0, t * w1, r, l, 8);
+    drawGear(x0 - r1 - r, y0, tp * w1, r1, l, 80);
+    drawGear(x0 - r1 - r, y0, tp * w1, r, l, 8);
 
-    drawGear(x0, y0, t * w2, r2, l, 62);
-    drawGear(x0, y0, t * w2, r, l, 8);
+    drawGear(x0 + r1 + r, y0, tp * w1, r1, l, 80);
+    drawGear(x0 + r1 + r, y0, tp * w1, r, l, 8);
 
-    drawGear(x0, y0 - r3 - r, t * w3, r3, l, 62);
-    drawGear(x0, y0 - r3 - r, t * w3, r, l, 8);
+    drawGear(x0, y0, tp * w2, r2, l, 62);
+    drawGear(x0, y0, tp * w2, r, l, 8);
 
-    drawGear(x0, y0, t * w4, r4, l, 48);
-    drawGear(x0, y0, t * w4, r, l, 8);
+    drawGear(x0, y0 - r3 - r, tp * w3, r3, l, 62);
+    drawGear(x0, y0 - r3 - r, tp * w3, r, l, 8);
 
-    drawGear(x0, y0 + r4 + r, t * wg, rg, l, 20);
-    drawGear(x0, y0 + r4 + r, t * wg, r, l, 8);
+    drawGear(x0, y0, tp * w4, r4, l, 48);
+    drawGear(x0, y0, tp * w4, r, l, 8);
 
-    drawPendulum(x0, y0 + r4 + rg + r, rp, 3 * M_PI_2 + 0.2 * sin(t * wp));
+    drawGear(x0 + r5 + r, y0, tp * w5, r5, l, 28);
+    drawGear(x0 + r5 + r, y0, tp * w5, r, l, 8);
+
+    drawGear(x0, y0, tp * w6, r6, l, 28);
+    drawGear(x0, y0, tp * w6, r, l, 8);
+
+    drawGangi(x0, y0 + r4 + r, tp * wg, rg, 0.02, 20);
+    drawGear(x0, y0 + r4 + r, tp * wg, r, l, 8);
+
+    drawPendulum(x0, y0 + r4 + rg + r + 0.01, rp, 3 * M_PI_2 + 0.1 * sin(t * wp));
 
     glFlush();
 }
@@ -129,18 +151,11 @@ void Reshape(int w, int h) {
 // タイマー
 void Timer(int value) {
     struct timeval tv;
-    struct tm *t_st;
 
     glutTimerFunc(1000.0 / FPS, Timer, 0);
 
     // 時刻を取得
     gettimeofday(&tv, NULL);
-    t_st = localtime(&tv.tv_sec);
-
-    sec = t_st->tm_sec + tv.tv_usec * 1e-6;
-    min = t_st->tm_min + sec / 60;
-    hour = t_st->tm_hour + min / 60;
-
     t = tv.tv_sec + tv.tv_usec * 1e-6;
 
     Display();
@@ -150,7 +165,7 @@ void Timer(int value) {
 void drawCircle(double x, double y, double r, int n) {
     int i;
 
-    glBegin(GL_LINE_LOOP);
+    glBegin(GL_POLYGON);
 
     for (i = 0; i < n; i++) {
         double theta = 2 * M_PI * i / n;
@@ -162,6 +177,39 @@ void drawCircle(double x, double y, double r, int n) {
 
 // 歯車
 void drawGear(double x, double y, double a, double r, double l, int n) {
+    int i;
+    double w = M_PI / n;
+
+    drawCircle(x, y, r - l / 2, n * 2);
+
+    glBegin(GL_QUADS);
+
+    for (i = 0; i < n * 2; i += 2) {
+        glVertex2d(x + (r - l / 2) * cos(i * w + a), y + (r - l / 2) * sin(i * w + a));
+        glVertex2d(x + (r - l / 2) * cos(i * w + a) + l * cos((i + 0.5) * w + a), y + (r - l / 2) * sin(i * w + a) + l * sin((i + 0.5) * w + a));
+        glVertex2d(x + (r - l / 2) * cos((i + 1) * w + a) + l * cos((i + 0.5) * w + a), y + (r - l / 2) * sin((i + 1) * w + a) + l * sin((i + 0.5) * w + a));
+        glVertex2d(x + (r - l / 2) * cos((i + 1) * w + a), y + (r - l / 2) * sin((i + 1) * w + a));
+    }
+
+    glEnd();
+
+    /*glBegin(GL_LINE_LOOP);
+
+    for (i = 0; i < 2 * n; i++) {
+        if (i % 2) {
+            glVertex2d(x + (r - l / 2) * cos(i * w + a), y + (r - l / 2) * sin(i * w + a));
+            glVertex2d(x + (r - l / 2) * cos((i + 1) * w + a), y + (r - l / 2) * sin((i + 1) * w + a));
+        } else {
+            glVertex2d(x + (r - l / 2) * cos(i * w + a) + l * cos((i + 0.5) * w + a), y + (r - l / 2) * sin(i * w + a) + l * sin((i + 0.5) * w + a));
+            glVertex2d(x + (r - l / 2) * cos((i + 1) * w + a) + l * cos((i + 0.5) * w + a), y + (r - l / 2) * sin((i + 1) * w + a) + l * sin((i + 0.5) * w + a));
+        }
+    }
+
+    glEnd();*/
+}
+
+// カナ (小さい歯車)
+void drawKana(double x, double y, double a, double r, double l, int n) {
     int i;
     double w = M_PI / n;
 
@@ -180,6 +228,25 @@ void drawGear(double x, double y, double a, double r, double l, int n) {
     glEnd();
 }
 
+// ガンギ車 
+void drawGangi(double x, double y, double a, double r, double l, int n) {
+    int i;
+    double w = -2 * M_PI / n;
+
+    drawCircle(x, y, r - l / 2, n);
+
+    glBegin(GL_TRIANGLES);
+
+    for (i = 0; i < n; i++) {
+        glVertex2d(x + (r - l / 2) * cos(i * w + a), y + (r - l / 2) * sin(i * w + a));
+        glVertex2d(x + (r + l / 2) * cos(i * w + a), y + (r + l / 2) * sin(i * w + a));
+        glVertex2d(x + (r - l / 2) * cos((i + 1) * w + a), y + (r - l / 2) * sin((i + 1) * w + a));
+    }
+
+    glEnd();
+}
+
+// 針
 void drawHand(double x, double y, double r, double a) {
     glBegin(GL_LINES);
     glVertex2d(x, y);
@@ -187,11 +254,22 @@ void drawHand(double x, double y, double r, double a) {
     glEnd();
 }
 
+// 振り子
 void drawPendulum(double x, double y, double r, double a) {
+    // 棒
     glBegin(GL_LINES);
-    glVertex2d(x, y);
+    glVertex2d(x - 0.1 * cos(a), y - 0.1 * sin(a));
     glVertex2d(x + r * cos(a), y + r * sin(a));
     glEnd();
 
-    drawCircle(x + r * cos(a), y + r * sin(a), 0.1, 100);
+    // 重り
+    drawCircle(x + (r + 0.1) * cos(a), y + (r + 0.1) * sin(a), 0.1, 100);
+
+    // アンクル
+    glBegin(GL_LINE_STRIP);
+    glVertex2d(x - 0.08 * sin(a) + 0.02 * cos(a), y + 0.08 * cos(a) + 0.02 * sin(a));
+    glVertex2d(x - 0.08 * sin(a), y + 0.08 * cos(a));
+    glVertex2d(x + 0.08 * sin(a), y - 0.08 * cos(a));
+    glVertex2d(x + 0.08 * sin(a) + 0.02 * cos(a), y - 0.08 * cos(a) + 0.02 * sin(a));
+    glEnd();
 }
